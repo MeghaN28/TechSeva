03187a022f702f1c9fd52e53cc939263a88bbc3d
MeghaN28 <n.megha82@gmail.com>
2021-07-27 17:55:26 +0530

    Added images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
-const home = () => {
+import IMAGES from '../images/image';
+function home() {
     return (
        <div>
           <h3>Providing The Online Services to our Customers With All Features</h3>
-           <p>Aadhar</p>
-           <p>Free PAN</p>
+           
+           <img src={IMAGES.logoServices} alt=""></img>
+           <img src={IMAGES.logoRationCard} alt=""></img>
+           <img src={IMAGES.logoVoterId} alt=""></img>
+           <img src={IMAGES.logoVoterService} alt=""></img>
+           <img src={IMAGES.logoPartTime} alt=""></img>
+           <img src={IMAGES.logoNovel} alt=""></img>
+           <img src={IMAGES.logoLandServices} alt=""></img>
+           <img src={IMAGES.logoGovtServices} alt=""></img>
+           <img src={IMAGES.logoGovtJob} alt=""></img>
        </div>
+            
+       
     );
 }
  
---
+++ src/images/image.js
@@ -0,0 +1,13 @@
+const IMAGES={
+    logoServices:require('./e_services.jpg').default,
+    logoGovtJob:require('./govt_job_alert.jpg').default,
+    logoLandServices:require('./govt_land_services.jpg').default,
+    logoGovtServices:require('./govt_services.jpg').default,
+    logoNovel:require('./novel_book.jpg').default,
+    logoPartTime:require('./part_time_jobs.jpg').default,
+    logoRationCard:require('./ration_card.jpg').default,
+    logoVoterId:require('./voter_id.jpg').default,
+    logoVoterService:require('./voters_service_portal.jpg').default
+
+}
+export default IMAGES;
