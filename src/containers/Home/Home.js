538b78907f3fd5f173ef2e87c434a39fd6d2ad5d
MeghaN28 <n.megha82@gmail.com>
2021-07-27 19:20:35 +0530

    added css

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IMAGES from '../../images/index';
+import './Home.css';
 //import Header from '../../components/Header/Header';
 function home() {
     return (
@@ -7,8 +8,9 @@ function home() {
        <div>
            
           <h3>Providing The Online Services to our Customers With All Features</h3>
-           
-           <img src={IMAGES.logoServices} alt=""></img>
+          <div class="item">
+               <img src={IMAGES.logoServices} alt=""></img>
+               <figcaption class="caption">Services</figcaption>
            <img src={IMAGES.logoRationCard} alt=""></img>
            <img src={IMAGES.logoVoterId} alt=""></img>
            <img src={IMAGES.logoVoterService} alt=""></img>
@@ -17,6 +19,7 @@ function home() {
            <img src={IMAGES.logoLandServices} alt=""></img>
            <img src={IMAGES.logoGovtServices} alt=""></img>
            <img src={IMAGES.logoGovtJob} alt=""></img>
+       </div>
        </div>
             
        
